602f3d5162f20847db64979d31b0334d0b7826d3
BipinChand-git <bipinchand616@gmail.com>
2025-05-02 00:40:36 +0530

    Persisting Data With Local Storage

--- src/app/dummy-users.ts
+++ src/app/dummy-users.ts
@@ -1,32 +1,32 @@
 export const DUMMY_USERS = [
     {
       id: 'u1',
-      name: 'Jasmine Washington',
+      name: 'Bipin Chand',
       avatar: 'user-1.jpg',
     },
     {
       id: 'u2',
-      name: 'Emily Thompson',
+      name: 'Harsh Yadav',
       avatar: 'user-2.jpg',
     },
     {
       id: 'u3',
-      name: 'Marcus Johnson',
+      name: 'Govind Raghav',
       avatar: 'user-3.jpg',
     },
     {
       id: 'u4',
-      name: 'David Miller',
+      name: 'Lokesh Khatri',
       avatar: 'user-4.jpg',
     },
     {
       id: 'u5',
-      name: 'Priya Patel',
+      name: 'Piyush Jha',
       avatar: 'user-5.jpg',
     },
     {
       id: 'u6',
-      name: 'Arjun Singh',
+      name: 'Priya Patel',
       avatar: 'user-6.jpg',
     },
 ];
--- src/app/tasks/tasks.service.ts
+++ src/app/tasks/tasks.service.ts
@@ -14,22 +14,29 @@ export class TasksService {
             userId: 'u1',
             title: 'Master Angular',
             summary: 'Learn all the basic and advanced features of Angular and how to apply them.',
-            dueDate: '2025-06-01',
+            dueDate: '2025-06-10',
         },
         {
             id: 't2',
             userId: 'u3',
-            title: 'Build first prototype',
-            summary: 'Build a first prototype of the online shop website',
+            title: 'Academic Project',
+            summary: 'Create a frontend for our Event-Booking Project using React',
             dueDate: '2025-06-30',
         },
         {
             id: 't3',
-            userId: 'u3',
+            userId: 'u5',
             title: 'Prepare issue template',
             summary: 'Prepare and describe an issue template which will help with project management',
             dueDate: '2025-06-15',
         },
+        {
+            id: 't4',
+            userId: 'u2',
+            title: 'Learn DSA',
+            summary: 'Learn advanced Data Structures and Algorithms using Java',
+            dueDate: '2025-06-10',
+        }
     ];
 
     // Using localStorage to persist my data.
